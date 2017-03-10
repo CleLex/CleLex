77438f244e91ea81414190546404fcb77afcba4c
JaoChaos <juan.anaya.ortiz@gmail.com>
2017-03-10 12:52:40 +0100

    Add files via upload

---
+++ tests/tabulartest.cpp
@@ -0,0 +1,24 @@
+/////////////////////////////////////////////////////////
+
+//Archivo de test para probar la función de tabulación.
+
+/////////////////////////////////////////////////////////
+
+#include<iostream>
+
+int main(){
+
+int x, y;
+
+			int suma=0;
+
+cout << "Introduzca los dos valores a sumar:" << "\n";
+cin >> x >> y ;
+
+suma = x+y;
+
+cout << "El valor de la suma es: " << suma;
+
+system("pause");
+
+}
